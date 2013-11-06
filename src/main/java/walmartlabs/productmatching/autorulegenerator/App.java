75cf5405c645b539f798303b04ab334ec2666e08
Shishir Kumar Prasad <skprasad@cs.wisc.edu>
2013-11-05 20:19:42 -0800

    Initial commit

---
+++ src/main/java/walmartlabs/productmatching/autorulegenerator/App.java
@@ -0,0 +1,13 @@
+package walmartlabs.productmatching.autorulegenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
